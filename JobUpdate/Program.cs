8627b89e4b409e70550516f92ca21e38f1451c9b
SHORYA VIJAYVARGIYA <shoryavijayvargiya@SHORYAs-MacBook-Air.local>
2019-11-15 20:36:09 +0530

    jng

---
+++ JobUpdate/Program.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace JobUpdate
+
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            
+//            string[] abc = args;
+
+            Console.WriteLine("Heloooooo");
+            for (int i=0;i<args.Length;i++)
+            {
+//          Console.WriteLine(args[i]);
+
+          var jsonArray = JArray.Parse(args[i]);
+
+          Console.WriteLine(jsonArray.GetType());
+          Console.WriteLine(jsonArray);
+                Console.WriteLine(jsonArray.GetType());
+
+
+            }
+
+            //    Console.WriteLine(jsonArray[1][0]);
+            // var Content = args[0]["Content"];
+            //var Path = args[0]["Path"];
+
+
+
+            //   var Content = jsonArray[0]["Content"];
+            // var Path = jsonArray[0]["Path"];
+
+            //string abs = Content.ToString();
+            //string abs2 = Path.ToString();
+            //Console.WriteLine($"{Path}");
+            // string root = ("./../" + abs2);
+            //// Console.Writeline(Content) ;
+            ////Console.Writeline(Path) ;
+            ////Console.Writeline(root) ;
+
+            //// This text is added only once to the file.
+
+            //// Create a file to write to.
+            //  File.WriteAllText(root, abs);
+            //   Console.WriteLine(abs);
+
+            //Open the file to read from.
+            //  string readText = File.ReadAllText(root);
+
+            // Console.WriteLine(readText);
+        }
+
+
+    }
+
+
+
+}
